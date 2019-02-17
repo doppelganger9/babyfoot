43e5630c5291eb526eb3bb0294f5a2b62238a720
David Lacourt <david.lacourt+github@gmail.com>
2019-02-17 14:35:18 +0000

    Fixed script to hopefully fix the build

--- wait-until-server-ready.sh
+++ wait-until-server-ready.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 # wait-until-server-ready.sh
 # looks for a specific string to appear in a log before exiting.
+# waits for the log file to appear.
+# try this a few times before giving up (max number of retries).
 #
 # how to use:
 #
@@ -10,15 +12,24 @@
 # run whatever you want, like integration tests
 # kill %1 to stop the background process.
 
-echo "Waiting for server to be ready..."
-while true ; do
-  echo "."
-  result=$(grep -nE 'server listening on port' server.log) # -n shows line number
-  echo "DEBUG: Result found is $result"
-  if [ -z $result ] ; then
-    echo "READY!"
-    break
+echo "Waiting for server.log to appear and contain a line with 'server listing on port' in it..."
+number_of_test=1
+MAX_NB_RETRIES=50
+while [ $number_of_test -le $MAX_NB_RETRIES ]; do
+  echo "$number_of_test"
+  if [ -f server.log ]; then
+    result=$(grep -nE 'server listening on port' server.log) # -n shows line number
+    echo "DEBUG: Result found is '$result'"
+    if [ "$result" != "" ]; then
+        echo "READY!"
+        break
+    else
+        let number_of_test=number_of_test+1
+    fi
+  else
+    let number_of_test=number_of_test+1
+    echo "DEBUG: server.log not found..."
   fi
   sleep 1
 done
-
+echo "finished after trying $(($number_of_test-1)) times"

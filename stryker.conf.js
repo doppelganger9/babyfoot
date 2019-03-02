e35aaf82169bd871cf2bbb7938bc06615ff7f67a
David Lacourt <david.lacourt+github@gmail.com>
2019-03-02 22:25:07 +0000

    fixed gitignore and add stryker config (was ignored)

---
+++ stryker.conf.js
@@ -0,0 +1,20 @@
+module.exports = function(config) {
+  config.set({
+    mutator: "typescript",
+    packageManager: "npm",
+    reporters: ["html", "clear-text", "progress", "dashboard"],
+    testRunner: "mocha",
+    transpilers: ["typescript"],
+    testFramework: "mocha",
+    coverageAnalysis: "perTest",
+    tsconfigFile: "test/tsconfig.json",
+    mutate: ["src/**/*.ts", "!src/**/*.spec.ts"],
+    mochaOptions: {
+        opts: 'test/mocha.opts',
+        files: "dist/**/*.spec.js",
+        ui: 'bdd',
+        timeout: 3000,
+        grep: /.*/
+    }
+  });
+};

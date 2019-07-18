317c2cf5fe0a9890582fe200b479719b94bf9bd5
David Lacourt <david.lacourt+github@gmail.com>
2019-07-18 20:39:19 +0000

    Fixed code quality + added unit tests for new route. needed a mock lib: sinonJS

---
+++ src/routes/uptime-routes.spec.ts
@@ -0,0 +1,55 @@
+import { expect } from 'chai';
+import * as sinon from 'sinon';
+
+import { EventPublisher, BFEventsStore } from '..';
+import { UptimeRoutes } from './uptime-routes';
+import { Router, Request, Response } from 'express';
+
+describe('Uptime Routes', () => {
+  let t: UptimeRoutes;
+
+  beforeEach(() => {
+    t = new UptimeRoutes();
+  })
+
+  it('should initialize', () => {
+    expect(t).not.to.be.undefined;
+  });
+
+  it('should register route /api/health', () => {
+    const mockRouter = {get: sinon.fake() as any} as Router;
+
+    t.registerRoutes(mockRouter);
+
+    expect((mockRouter.get as sinon.SinonSpy).calledOnce).to.be.true;
+    expect((mockRouter.get as sinon.SinonSpy).firstCall.args[0]).to.equal('/api/health');
+  });
+
+  it('should have a health route that returns HTTP 200 OK with an expected JSON payload', () => {
+    const mockReq = {} as unknown as Request;
+    let memo: any = {};
+    const mockRes = new MockResponse(memo) as unknown as Response;
+
+    t.health(mockReq, mockRes);
+
+    expect(memo.status).to.equal(200);
+    expect(JSON.stringify(memo.send)).to.equal('{"status":"OK"}');
+  });
+
+});
+
+class MockResponse {
+  constructor(public memo: any) {
+    this.memo = memo;
+  }
+
+  send(obj: any): MockResponse {
+    this.memo['send'] = obj;
+    return this;
+  }
+
+  status(code: number): MockResponse {
+    this.memo['status'] = code;
+    return this;
+  }
+}
